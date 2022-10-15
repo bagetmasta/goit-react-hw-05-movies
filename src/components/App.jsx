fc25bfcf3f84282f497be3e63ced6c4b715ad79e
Artem Bahmet <baget.masta@gmail.com>
2022-10-15 12:04:32 +0300

    check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
+import Movies from 'pages/Movies/Movies';
 import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/SharedLayout/SharedLayout';
 import Home from '../pages/Home/Home';
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,9 @@
+const Movies = () => {
+  return (
+    <>
+      <p>Страница поиска фильмов по ключевому слову</p>
+    </>
+  );
+};
+
+export default Movies;
